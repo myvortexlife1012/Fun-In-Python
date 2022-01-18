0608a254ac7895fb32a625fe007c53b35b0d910a
myvortexlife1012 <92078374+myvortexlife1012@users.noreply.github.com>
2022-01-18 14:26:13 -0700

    Updated clipboardTEXT for Linux also
    there was stuff you had to install in linux

--- ClipboardTEXT.py
+++ ClipboardTEXT.py
@@ -1,12 +1,33 @@
 #import ClipboardTEXT as ctxt
-#ctxt.clipboard()
+#text = ctxt.ClipboardTEXT()
 
-def clipboard():
-  import win32clipboard
+# Required for Linux - 1 of:
+# sudo apt-get install xsel
+# sudo apt-get install xclip
+# pip install gtk
+# pip install PyQt4
 
-  # get clipboard data
-  win32clipboard.OpenClipboard()
-  data = win32clipboard.GetClipboardData()
-  win32clipboard.CloseClipboard()
-  print(data)
-  return data
+def ClipboardTEXT():
+  import platform
+  pl = platform.system()
+
+  if pl == 'Linux':
+    print("You're using Linux")
+    import clipboard
+    #clipboard.copy("abc")  # now the clipboard content will be string "abc"
+    text = clipboard.paste()  # text will have the content of clipboard
+    print("clipboard has:")
+    print(text)
+    return text
+
+  if pl == 'Windows':
+    import win32clipboard
+
+    # get clipboard data
+    win32clipboard.OpenClipboard()
+    data = win32clipboard.GetClipboardData()
+    win32clipboard.CloseClipboard()
+    print("clipboard has:")
+    print(data)
+    return data
+
