93b57e57bbe4e455ae400334a6370c9697e719da
myvortexlife1012 <92078374+myvortexlife1012@users.noreply.github.com>
2022-01-18 14:30:53 -0700

    Updated this file

--- ClipboardCHANGES.py
+++ ClipboardCHANGES.py
@@ -1,20 +1,36 @@
+#v1
+
+#NOTE: this is a loop
+#when the clipboard changes
+#it prints it in the console
 
 #import ClipboardCHANGES as cchanges
 #cchanges.ClipboardCHANGES()
 
+# Required for Linux - 1 of:
+# sudo apt-get install xsel
+# sudo apt-get install xclip
+# pip install gtk
+# pip install PyQt4
+
 def ClipboardCHANGES():
-  import time
-  import sys
-  import os
-  sys.path.append(os.path.abspath("SO_site-packages"))
+  import platform
+  pl = platform.system()
 
-  import pyperclip
+  if pl == 'Windows' or pl == 'Linux':
+    import time
+    import sys
+    import os
+    sys.path.append(os.path.abspath("SO_site-packages"))
 
-  recent_value = ""
-  while True:
-    tmp_value = pyperclip.paste()
-    if tmp_value != recent_value:
-      recent_value = tmp_value
-      print("Value changed: %s" % str(recent_value))
-      #print("Value changed: %s" % str(recent_value)[:20]) #just 20 chars
-    time.sleep(0.1)
+    import pyperclip
+
+    recent_value = ""
+    while True:
+      tmp_value = pyperclip.paste()
+      if tmp_value != recent_value:
+        recent_value = tmp_value
+        print("Value changed: %s" % str(recent_value))
+        #print("Value changed: %s" % str(recent_value)[:20]) #just 20 chars
+      time.sleep(0.1)
+
