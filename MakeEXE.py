58ffff8060a04a3ea6a7e6f04cac24390302703d
myvortexlife1012 <92078374+myvortexlife1012@users.noreply.github.com>
2022-01-29 15:27:11 -0700

    updated making the exe - for linux and windows
    updated making the exe - for linux and windows

--- MakeEXE.py
+++ MakeEXE.py
@@ -1,4 +1,8 @@
 
+#-----------------------------
+
+#FOR WINDOWS:
+
 # make exe for the background change app
 # pip install auto-py-to-exe # type 'auto-py-to-exe' in the console ... gui appears
 
@@ -6,3 +10,29 @@
 # make exe for the background change app
 # pip install pyinstaller
 # pyinstaller --onefile hello.py # makes the exe file (in dist folder)
+
+
+#-----------------------------
+
+#FOR LINUX:
+
+"""
+Just put this in the first line of your script :
+
+#!/usr/bin/env python
+Make the file executable with
+
+chmod +x myfile.py
+Execute with
+
+./myfile.py
+
+
+----
+
+or
+
+try  pyinstaller
+
+
+"""
